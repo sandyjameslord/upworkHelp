e5a51f0a4cd61727d23bfe3e4ce896f3b8643183
Sandy Lord <sandyjameslord@gmail.com>
2021-06-30 14:10:24 -0400

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  webCirc
+//
+//  Created by Sandy Lord on 6/21/21.
+//
+
+import UIKit
+import Foundation
+
+struct APIResponse: Codable {
+    let _id: APIID?
+    let abQuestion: String?
+    let abQuestionDomain: String?
+    let abSentence: String?
+    let bcQuestion: String?
+    let bcQuestionDomain: String?
+    let bcSentence: String?
+    let imageName: String?
+    let numSyll: APINumSyll?
+    let photo: String?
+    let progName: String?
+    let syllStructure: String?
+    let theme: String?
+    let unit: APIUnit?
+    let vocabulary: String?
+}
+    
+// individual structures for parts of the APIResponse
+struct APIUnit: Codable {
+    let numberInt: Int
+}
+struct APINumSyll: Codable {
+    let numberInt: Int
+}
+struct APIID: Codable {
+    let oid: String
+}
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        getData{(APIResponse) in
+            
+            print("APIResponse: ", APIResponse) // prints nothing, as the APIResponse returned out of the guard let statement
+        }
+
+    }
+
+    func getData(_ completion: @escaping ((APIResponse?) -> ())) {
+        let urlPath = "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/cirdata-khyvx/service/cirData/incoming_webhook/cirlAPI"
+
+        guard let url = URL(string: urlPath) else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            print("data",data) // some 304000 bytes, indicating success
+
+            do {
+                if let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSArray {
+                    print("jsonResult", jsonResult) // prints a copy of the database to use, eg., for debugging, also indicating success
+                }
+            } catch {
+            }
+
+            // Now I would like to use JSONDecoder() to decode the data following the APIResponse struct defined on lines 11-27.
+            do {
+                                        
+                guard let APIResponse = try? JSONDecoder().decode(APIResponse.self, from: data) else { return }
+                print("APIResponse", APIResponse) // nothing prints here, so the JSONDecoder must not be able to decode. It is probably my struct that is not formatted correctly.
+                completion(APIResponse)
+            } catch {
+                            //Catch Error here...
+            }
+        }
+        task.resume()
+    }
+}
+
